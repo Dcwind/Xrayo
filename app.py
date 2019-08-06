d1d85130aa3f76e2f3758bd89d64de0d96cc4a3d
Daniel Ekwuazi <danielekwuazi@gmail.com>
2019-08-06 15:26:30 +0700

    init

---
+++ app.py
@@ -0,0 +1,127 @@
+from __future__ import division, print_function
+# coding=utf-8
+# import os
+# import numpy as np
+# import cv2
+# import matplotlib.pyplot as plt
+# from tensorflow.keras.models import load_model
+
+# Keras
+# from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+# ResNet50
+# from keras.applications.resnet50 import ResNet50
+# from keras.applications.imagenet_utils import preprocess_input as preprocess_input_resNet50, decode_predictions as decode_predictions_resNet50
+# # VGG16
+# from keras.applications.vgg16 import VGG16
+# from keras.applications.vgg16 import preprocess_input as preprocess_input_vgg16, decode_predictions as decode_predictions_vgg16
+# # Xception
+# from keras.applications.xception import Xception
+# from keras.applications.xception import preprocess_input as preprocess_input_xception, decode_predictions as decode_predictions_xception
+
+# Define a flask app
+app = Flask(__name__)
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+
+# model = load_model("./models/3-conv-128-layer-dense-1-out-2-softmax-categorical-cross-2-CNN.model")
+
+# modelResNet50 = ResNet50(weights='imagenet')
+# print('ResNet50 Model loaded.')
+
+# modelVGG16 = VGG16(weights='imagenet', include_top=True)
+# print('VGG16 Model loaded.')
+
+# modelXception = Xception(weights='imagenet', include_top=True)
+# print('Xception Model loaded.')
+
+print('Running on http://localhost:5000')
+
+def get_file_path_and_save(request):
+    # Get the file from post request
+    f = request.files['file']
+
+    # Save the file to ./uploads
+    basepath = os.path.dirname(__file__)
+    file_path = os.path.join(
+        basepath, 'uploads', secure_filename(f.filename))
+    f.save(file_path)
+    return file_path
+
+def prepare(filepath):
+    img_size = 150  
+    img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)/255  
+    img_resize = cv2.resize(img, (img_size, img_size))  
+    return np.array(img_resize).reshape(-1, img_size, img_size, 1) 
+    
+
+def c_scoref(vals):
+    print(vals)
+    #confidence score
+    # [Dog, Cat]    
+
+    if vals[1] > vals[0]:
+        prediction = "PNEUMONIA"
+        c_score = vals[1]/sum(vals) * 100
+    else:
+        prediction = "NORMAL"
+        c_score = vals[0]/sum(vals) * 100
+    print("Prediction:", prediction)
+    print("Confidence score:",str(round(c_score,2)) + "%\n")
+    return (prediction, c_score)
+
+
+@app.route('/predictVGG16', methods=['GET', 'POST'])
+def predictVGG16():
+    if request.method == 'POST':
+        file_path = get_file_path_and_save(request)
+
+        prediction=model.predict(prepare(file_path))
+        v = prediction[0]
+        result = c_scoref(v)
+
+        # decode the results into a list of tuples (class, description, probability)
+        return str(result[0])
+    return None
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predictResNet50', methods=['GET', 'POST'])
+def predictResNet50():
+    if request.method == 'POST':
+        file_path = get_file_path_and_save(request)
+
+        img = image.load_img(file_path, target_size=(224, 224))
+
+        # Preprocessing the image
+        x = image.img_to_array(img)
+        # x = np.true_divide(x, 255)
+        x = np.expand_dims(x, axis=0)
+
+        # Be careful how your trained model deals with the input
+        # otherwise, it won't make correct prediction!
+        x = preprocess_input_resNet50(x, mode='caffe')
+        # Make prediction
+        preds = modelResNet50.predict(x)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = decode_predictions_resNet50(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    # Serve the app with gevent
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
